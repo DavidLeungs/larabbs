0600a0693723385462f8cc237977bbd78d9d5bcb
vagrant <vagrant@homestead>
2018-03-06 09:44:53 +0000

    Sidebar active users

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,12 +10,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    /*use Traits\LastActivedAtHelper;
-    use Traits\ActiveUserHelper;*/
-    /*use HasRoles;
-    use Notifiable {
-        notify as protected laravelNotify;
-    }*/
+    use Traits\ActiveUserHelper;
 
     use HasRoles;
 
